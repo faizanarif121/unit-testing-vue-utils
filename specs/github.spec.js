11cfec52a4a25a9918ca72e6bfda3f417504e3d5
Faizan Arif <faizanarif121@gmail.com>
2020-06-11 17:52:55 +0500

    work on methods

--- specs/github.spec.js
+++ specs/github.spec.js
@@ -1,6 +1,27 @@
 import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
+import { TestScheduler } from 'jest'
 
 describe('methods', () => {
+  test('compose url', () => {
+    const { composeUrl } = GithubCard.methods
+    expect(composeUrl(123)).toBe('https://api.github.com/users/123')
+  })
 
+  test('fetch data From API', async () => {
+    const jsonMock = jest.fn().mockResolvedValue('Github Resolved Value')
+    window.fetch = jest.fn().mockResolvedValue({
+      json: jsonMock,
+    })
+
+    const wrapper = mount(GithubCard, {
+      methods: {
+        composeUrl: () => 'url',
+      },
+    })
+    await wrapper.vm.fetchData()
+    expect(window.fetch).toHaveBeenCalledWith('url')
+    expect(jsonMock).toHaveBeenCalled()
+    expect(wrapper.vm.data).toBe('Github Resolved Value')
+  })
 })
--- src/github-card.vue
+++ src/github-card.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <form @submit.prevent="fetchData">
-      <input type="text" v-model="username">
+      <input type="text" v-model="username" />
       <button type="submit">Load Details...</button>
     </form>
 
     <template v-if="data">
       <div class="card">
-        <img :src="data.avatar_url" :alt="data.name">
+        <img :src="data.avatar_url" :alt="data.name" />
         <h2>{{ data.name }}</h2>
         <h4>{{ data.bio }} -- {{ data.location }}</h4>
       </div>
@@ -19,10 +19,10 @@
 export default {
   data() {
     return {
-      username: "",
+      username: '',
       data: undefined,
-      url: ''
-    };
+      url: '',
+    }
   },
 
   methods: {
@@ -32,9 +32,9 @@ export default {
 
     async fetchData() {
       this.url = this.composeUrl(this.username)
-      const response = await fetch(this.url);
-      this.data = await response.json();
-    }
-  }
-};
+      const response = await fetch(this.url)
+      this.data = await response.json()
+    },
+  },
+}
 </script>
