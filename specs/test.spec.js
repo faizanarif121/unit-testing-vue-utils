7ffb636161ec00bdaf6f1119a2efc40a9480508e
Faizan Arif <faizanarif121@gmail.com>
2020-06-11 12:26:48 +0500

    shallow mount and mount difference

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,5 +1,6 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import TestComponent from '@/test'
+import List from '@/list'
 
 test('mount a vue component', () => {
   const wrapper = mount(TestComponent, {
@@ -8,4 +9,16 @@ test('mount a vue component', () => {
     },
   })
   expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('Mount a Parent component', () => {
+  const wrapperUsingMount = mount(List)
+
+  expect(wrapperUsingMount.html()).toMatchSnapshot()
+})
+
+test('Shallow mount a Parent component', () => {
+  const wrapperUsingShallowMount = shallowMount(List)
+
+  expect(wrapperUsingShallowMount.html()).toMatchSnapshot()
 })
